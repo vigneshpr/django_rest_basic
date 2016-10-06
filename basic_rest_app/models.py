994cbb7085548b47294509df2a3974fe83aa7cbc
vigneshpr <vigneshwarann@techaffinity.com>
2016-10-06 18:49:26 +0530

    edited littele bit

--- basic_rest_app/models.py
+++ basic_rest_app/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Student(models.Model):
-    firstname = models.CharField(max_length=100, blank=True, default='')
+    firstname = models.CharField(max_length=100, blank=True, unique=True)
     lastname = models.CharField(max_length=100, blank=True, default='')
-    dept = models.TextField()
+    dept = models.TextField(unique=True)
     gender = models.BooleanField(default=False)
     language = models.CharField(default='python', max_length=100)
     
