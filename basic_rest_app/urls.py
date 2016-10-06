e5f12e3bf1b5116246dd8b5673570c6fb268eadf
vigneshpr <vigneshwarann@techaffinity.com>
2016-10-06 18:27:22 +0530

    rest with request method

--- basic_rest_app/urls.py
+++ basic_rest_app/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from basic_rest_app import views	
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^lst/$', views.student_list),
-    url(r'^det/(?P<pk>[0-9]+)/$', views.student_detail),
+    url(r'^list/$', views.student_list),
+    url(r'^show/(?P<pk>[0-9]+)/$', views.student_detail),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
--- basic_rest_app/views.py
+++ basic_rest_app/views.py
@@ -1,55 +1,43 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from basic_rest_app.models import Student
-from basic_rest_app.serializers import StudentSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-	
-@csrf_exempt
-def student_list(request):
+@api_view(['GET', 'POST'])
+def student_list(request, format=None):
     
     if request.method == 'GET':
         basic_rest = Student.objects.all()
         serializer_rest_get = StudentSerializer(basic_rest, many=True)
-        return JSONResponse(serializer_rest_get.data)
+        return Response(serializer_rest_get.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer_rest_post = StudentSerializer(data=data)
+        serializer_rest_post = StudentSerializer(data=request.data)
         if serializer_rest_post.is_valid():
             serializer_rest_post.save()
-            return JSONResponse(serializer_rest_post.data, status=201)
-        return JSONResponse(serializer_rest_post.errors, status=400)      
+            return Response(serializer_rest_post.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def student_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def student_detail(request, pk, format=None):
    
     try:
         student_detail = Student.objects.get(pk=pk)
     except Student.DoesNotExist:
-        return HttpResponse(status=404)
+         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer_get = StudentSerializer(student_detail)
-        return JSONResponse(serializer_get.data)
+        return Response(serializer_get.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer_put = StudentSerializer(student_detail, data=data)
+        serializer_put = StudentSerializer(student_detail, data=request.data)
         if serializer_put.is_valid():
             serializer_put.save()
-            return JSONResponse(serializer_put.data)
-        return JSONResponse(serializer_put.errors, status=400)
+            return Response(serializer_put.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         student_detail.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
