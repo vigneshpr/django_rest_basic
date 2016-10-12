13f89cc8b685d47b48540443cec41335d6edd29e
vigneshpr <vigneshwarann@techaffinity.com>
2016-10-12 12:56:45 +0530

    modified using request methods

--- listapi/gener_app/views.py
+++ listapi/gener_app/views.py
@@ -1,25 +1,15 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from rest_framework import generics
 from django.core import serializers
 from gener_app.models import *
 from gener_app.serializers import *
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-from django.http import HttpResponse
+from rest_framework import status
+# from django.http import HttpResponse
 # Create your views here.
-
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
 
 class listget(generics.RetrieveUpdateAPIView):
 	queryset = gener_api.objects.all()
@@ -27,17 +17,19 @@ class listget(generics.RetrieveUpdateAPIView):
 
 # class listcreate(APIView): #post
 @csrf_exempt
+@api_view(('POST',))	
 def demo(request):
 	if request.method == 'POST':
-		data1 = JSONParser().parse(request)
-		print data1
-		data={'published_year': 1978, 'publication': 'Mathi', 'author': 'Selvi', 'bookname': 'CC', 'dept': 'SE'}
+		data = JSONParser().parse(request)
+		# print data1
+		# data={'published_year': 1978, 'publication': 'Mathi', 'author': 'Selvi', 'bookname': 'CC', 'dept': 'SE'}
 		serializer = apiSerializer(data=data)
         if serializer.is_valid():
         	serializer.save()
         	#print serializer.data
-        	return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)	
+        	return Response(serializer.data, status=status.HTTP_201_CREATED)
+        	
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)	
 
 class listdel(generics.DestroyAPIView): #delete
 	queryset = gener_api.objects.all()
